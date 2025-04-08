b237d5d10b7ad5d1412a04e29696c0b7a4eb279f
fundanurozturk <fundanur203@gmail.com>
2025-04-08 17:18:08 +0300

    unity notlarımı yükledim

---
+++ Assets/HelloWorld.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Bu fonksiyon, oyun baþladýðýnda bir kez çalýþýr.
+    void Start()
+    {
+        Debug.Log("Merhaba, Dünya!");
+    }
+
+    // Bu fonksiyon, her frame'de çalýþýr.
+    void Update()
+    {
+        // Buraya her frame'de çalýþacak kodlar ekleyebilirsiniz.
+    }
+}
